931e57e46d54a85039dcd9d40b64f471078a9181
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-06 18:55:18 +0000

    changing configuration to run the app on nitrous.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,9 +59,9 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 3000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
