9813def908d85c777829d7e6b956a441e0c4b25c
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-12 20:51:28 -0200

    updating teams logo

--- app/scripts/controllers/wizard-houses.js
+++ app/scripts/controllers/wizard-houses.js
@@ -3,14 +3,14 @@
 angular.module('magicSpellsApp')
   .controller('WizardHousesCtrl', function ($scope, $filter) {
     $scope.houses = [
-      { name: 'Eagles', picture: 'eagles.jpg' },
-      { name: 'Foxewe', picture: 'foxewe.jpg' },
-      { name: 'Horseal', picture: 'horseal.jpg' },
-      { name: 'Lioncow', picture: 'lioncow.jpg' },
-      { name: 'Monkeys in T-shirts', picture: 'monkeys_in_t-shirts.jpg' },
-      { name: 'Rubble Fish', picture: 'rubble_fish.jpg' },
-      { name: 'Russian Owls', picture: 'russian_owls.jpg' },
-      { name: 'Turkeywolf', picture: 'turkeywolf.jpg' }
+      { name: 'Eagles', picture: 'eagles.png' },
+      { name: 'Foxewe', picture: 'foxewe.png' },
+      { name: 'Horseal', picture: 'horseal.png' },
+      { name: 'Lioncow', picture: 'lioncow.png' },
+      { name: 'Monkeys in T-shirts', picture: 'monkeys_in_t-shirts.png' },
+      { name: 'Rubble Fish', picture: 'rubble_fish.png' },
+      { name: 'Russian Owls', picture: 'russian_owls.png' },
+      { name: 'Turkeywolf', picture: 'turkeywolf.png' }
     ];
 
     $scope.groupedHouses = $filter('group')($scope.houses, 4);
