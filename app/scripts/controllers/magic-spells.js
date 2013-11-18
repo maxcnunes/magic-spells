91770abe2dff9bf55173ee5473b5bbf47737805a
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-18 23:18:13 +0000

    improving "magic spells" page (need refactoring in the controller yet :/)

--- app/scripts/controllers/magic-spells.js
+++ app/scripts/controllers/magic-spells.js
@@ -1,5 +1,30 @@
 'use strict';
 
 angular.module('magicSpellsApp')
-  .controller('MagicSpellsCtrl', function ($scope) {
+  .controller('MagicSpellsCtrl', function ($scope, $location) {
+    var attempts = 0;
+    $scope.round = 1;
+    $scope.lockedListen = false;
+    $scope.attemptColor = 'primary';
+    $scope.listenFirstWord = function(event){
+      if(attempts === 0) {
+        $scope.attemptColor = 'danger';
+        attempts += 1; 
+      } else {
+        $scope.lockedListen = true;
+      }
+      
+      event.preventDefault();
+    };
+    
+    $scope.goToNextPage = function () {
+      if($scope.round < 10){
+        $scope.round += 1;
+        attempts = 0;
+        $scope.lockedListen = false;
+        $scope.attemptColor = 'primary';
+      } else {
+       $location.path("board"); 
+      }
+    };
   });
