09bfa8767de0491e251e7a8f46b05291cb685788
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-18 22:37:18 -0200

    adding google translate audios

--- app/scripts/controllers/magic-spells.js
+++ app/scripts/controllers/magic-spells.js
@@ -2,29 +2,50 @@
 
 angular.module('magicSpellsApp')
   .controller('MagicSpellsCtrl', function ($scope, $location) {
-    var attempts = 0;
+    var attempts = 0,
+        words = ['fight', 'magic'];
+
     $scope.round = 1;
     $scope.lockedListen = false;
     $scope.attemptColor = 'primary';
+    $scope.currentWord = words[0];
     $scope.listenFirstWord = function(event){
-      if(attempts === 0) {
+      attempts += 1;
+      if(attempts === 1) {
         $scope.attemptColor = 'danger';
-        attempts += 1; 
-      } else {
+        listen();
+      } else if (attempts === 2){
+        listen();
         $scope.lockedListen = true;
       }
-      
       event.preventDefault();
     };
-    
+
+    var listen = function(){
+        //
+        // TODO: convert it to a Directive
+        //
+
+        var player = document.getElementById('magic-spell-audio');
+
+        // stop previous audio
+        var audioPath = player.src;
+        player.src = '';
+        player.src = audioPath;
+
+        // play audio from the beginning
+        player.play();
+    };
+
     $scope.goToNextPage = function () {
       if($scope.round < 10){
+        $scope.currentWord = words[$scope.round];
         $scope.round += 1;
         attempts = 0;
         $scope.lockedListen = false;
         $scope.attemptColor = 'primary';
       } else {
-       $location.path("board"); 
+       $location.path("board");
       }
     };
   });
