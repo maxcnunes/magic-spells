8ca477eed2c1415dd33f6ce4fa3007303b76d57d
Max Claus Nunes <maxcnunes@gmail.com>
2013-11-19 17:17:50 +0000

    adding total labels

--- app/scripts/controllers/magic-board.js
+++ app/scripts/controllers/magic-board.js
@@ -2,25 +2,54 @@
 
 angular.module('magicSpellsApp')
 .controller('MagicBoardCtrl', function ($scope) {
-
-  var round = {
-    spell: { infinitive: "Do, Does", simplePast: "Did", pastParticiple: "Done"  },
-    results: [
-      { wizard: 'Max', correct: false, picture: '/images/users/max.jpg' },
-      { wizard: 'Rafael', correct: true, picture: '/images/users/vanessa.jpg' },
-      { wizard: 'Mitchel', correct: true, picture: '/images/users/mitchel.jpg' },
-      { wizard: 'Maycon', correct: true, picture: '/images/users/maycon.jpg' },
-      { wizard: 'Vanessa', correct: true, picture: '/images/users/rafael.jpg' }
-    ]
-  };
+  var fakeRound = function() {
+    return {
+      spell: { infinitive: "Do, Does", simplePast: "Did", pastParticiple: "Done"  },
+      results: [
+        { wizard: 'Max', correct: false, picture: '/images/users/max.jpg' },
+        { wizard: 'Rafael', correct: true, picture: '/images/users/rafael.jpg' },
+        { wizard: 'Mitchel', correct: true, picture: '/images/users/mitchel.jpg' },
+        { wizard: 'Maycon', correct: true, picture: '/images/users/maycon.jpg' },
+        { wizard: 'Vanessa', correct: true, picture: '/images/users/vanessa.jpg' }
+      ]
+    };
+  }
 
   var rounds = [];
-  rounds.push(round);
-  rounds.push(round);
-  rounds.push(round);
-  rounds.push(round);
-  rounds.push(round);
+  rounds.push(fakeRound());
+  rounds.push(fakeRound());
+  rounds.push(fakeRound());
+  rounds.push(fakeRound());
+  rounds.push(fakeRound());
 
 
   $scope.rounds = rounds;
+  
+  $scope.toggleCorrect = function(item){
+    item.correct = !item.correct;
+  };
+  
+  $scope.calculateWizardTotal = function(item){
+    var total = 0;
+    $scope.rounds.forEach( function(round) {
+      round.results.forEach( function(result) {
+        if(result.wizard === item.wizard && result.correct) {
+          total += 1;          
+        }
+      });
+    });
+    return total;
+  };
+  
+  $scope.calculateTotal = function(){
+    var total = 0;
+    $scope.rounds.forEach( function(round) {
+      round.results.forEach( function(result) {
+        if(result.correct) {
+          total += 1;          
+        }
+      });
+    });
+    return total;
+  };
 });
